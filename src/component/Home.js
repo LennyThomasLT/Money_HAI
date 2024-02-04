19ebd67f8c99a567496e46b939acbec4251840ec
Lenny Thomas <105914077+LennyThomasLT@users.noreply.github.com>
2024-02-04 23:02:35 +0700

    first commit

---
+++ src/component/Home.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Item from "./Items.js";
+import Form from "./Form.js";
+import { useEffect, useState } from "react";
+
+import "../css/style.css";
+import { DataContext } from "./DataContext.js";
+import { useContext } from "react";
+
+const design1 = { color: "red", textAlign: "center" };
+const Title = () => <h1 style={design1}>บัญชีรายรับรายจ่าย</h1>;
+const design2 = { color: "blue" };
+const Description = () => <h2 style={design2}>บันทึกข้อมูลในแต่ละวัน</h2>;
+const Transaction = (props) => {
+  const { items } = props;
+  const { income, expense } = useContext(DataContext);
+
+  return (
+    <div>
+      <div style={{ fontSize: "20px" }}>
+        {items.map((element) => {
+          return <Item {...element} key={element.id} />;
+        })}
+      </div>
+      <div>
+        <p>รายรับ : {income} บาท</p>
+        <p>รายจ่าย : {expense} บาท</p>
+        <p>คงเหลือ : {income - expense} บาท</p>
+      </div>
+    </div>
+  );
+};
+
+function Home() {
+  const [items, setItems] = useState([]);
+  const [reportIncome, setReportIncome] = useState(0);
+  const [reportExpense, setReportExpense] = useState(0);
+  const onAddNewItem = (newItem) => {
+    setItems((prevItem) => {
+      return [newItem, ...prevItem];
+    });
+  };
+
+  useEffect(() => {
+    const amounts = items.map((items) => items.amount);
+    const income = amounts
+      .filter((element) => element > 0)
+      .reduce((total, element) => (total += element), 0);
+    const expense =
+      amounts
+        .filter((element) => element < 0)
+        .reduce((total, element) => (total += element), 0) * -1;
+    setReportIncome(income);
+    setReportExpense(expense);
+  }, [items, reportIncome, reportExpense]);
+
+  return (
+    <DataContext.Provider
+      value={{ income: reportIncome, expense: reportExpense }}
+    >
+      <div className="font" style={{ padding: "30px" }}>
+        <Title />
+        <Form onAddItem={onAddNewItem} />
+        <Description />
+        <Transaction items={items} />
+      </div>
+    </DataContext.Provider>
+  );
+}
+
+export default Home;
